952d712eda628312819b282eec89cec7687ae684
rizqitsani <rizqitsani@gmail.com>
2021-11-16 18:03:41 +0700

    feat: add authentication
    added:
    - register
    - login
    - user info

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,17 +1,56 @@
 const express = require('express');
+const { StatusCodes } = require('http-status-codes');
 
-const controller = require('../controllers/auth.controller');
-const validate = require('../utils/validate');
+const { verifyToken } = require('../middlewares/auth');
+const validate = require('../middlewares/validate');
+const AuthService = require('../services/auth.service');
 
-const { login } = require('../validations/auth.validation');
+const { login, register } = require('../validations/auth.validation');
 
 const router = express.Router();
 
-router
-  .route('/login')
-  .get(controller.login)
-  .post(validate(login), controller.login);
+const authServiceInstance = new AuthService();
 
-router.route('/register').post(controller.register);
+router.route('/login').post(validate(login), async (req, res, next) => {
+  try {
+    const token = await authServiceInstance.login(req.body);
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: 'Success.', data: { token } });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.route('/register').post(validate(register), async (req, res, next) => {
+  try {
+    const data = await authServiceInstance.register(req.body);
+
+    return res.status(StatusCodes.OK).json({
+      message: 'Success.',
+      data,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.route('/info').post(verifyToken, async (req, res, next) => {
+  try {
+    return res.status(StatusCodes.OK).json({
+      message: 'Success.',
+      data: {
+        id: req.user.us_id,
+        name: req.user.us_nama,
+        email: req.user.us_email,
+        phone: req.user.us_no_hp,
+        address: req.user.us_alamat,
+      },
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
 
 module.exports = router;
---
+++ src/services/auth.service.js
@@ -0,0 +1,69 @@
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const { nanoid } = require('nanoid');
+
+const db = require('../config/db');
+const ApiError = require('../errors/ApiError');
+const { comparePassword } = require('../utils/auth');
+
+const UserService = require('./user.service');
+
+class AuthService {
+  constructor() {
+    this.User = db.sequelize.models.User;
+    this.UserService = new UserService();
+  }
+
+  async login(userCredentials) {
+    const user = await this.UserService.findUserByEmail(userCredentials.email);
+
+    if (!user) {
+      throw new ApiError({
+        status: StatusCodes.NOT_FOUND,
+        message: 'Pengguna tidak ditemukan!',
+      });
+    }
+
+    const isPasswordValid = await comparePassword(
+      userCredentials.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new ApiError({
+        status: StatusCodes.UNAUTHORIZED,
+        message: 'Password salah!',
+      });
+    }
+
+    const token = this.createToken(user.id);
+
+    return token;
+  }
+
+  async register(userData) {
+    const isEmailExist = await this.UserService.findUserByEmail(userData.email);
+
+    if (isEmailExist) {
+      throw new ApiError({
+        status: StatusCodes.BAD_REQUEST,
+        message: 'Email telah digunakan!',
+      });
+    }
+
+    const data = await db.sequelize.models.User.create({
+      id: nanoid(),
+      ...userData,
+    });
+
+    return data;
+  }
+
+  createToken(id) {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+  }
+}
+
+module.exports = AuthService;
