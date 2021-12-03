420d14094b7bcf934967e25fdba2d03d5de3f436
rizqitsani <rizqitsani@gmail.com>
2021-12-03 21:39:11 +0700

    feat: add product image upload

---
+++ src/config/multer.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const multer = require('multer');
+
+const imageStorage = multer.diskStorage({
+  destination: 'uploads/images',
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`,
+    );
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  limits: {
+    fileSize: 2000000, // 2 MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      return cb(new Error('Please upload a Image'));
+    }
+    return cb(undefined, true);
+  },
+});
+
+module.exports = {
+  imageUpload,
+};
--- src/server.js
+++ src/server.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
+app.use('/static', express.static('uploads'));
+
 db.sequelize
   .authenticate()
   .then(() => {
